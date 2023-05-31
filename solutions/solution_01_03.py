82fc39dd28382b4ae132791173acd104e3dcb159
WandrilleD <wandrille.duchemin@unibas.ch>
2023-05-31 09:50:22 +0200

    2023 course update

--- solutions/solution_01_03.py
+++ solutions/solution_01_03.py
@@ -3,10 +3,10 @@ k = 'box'
 category='canton name'
 represented_variable="60+ y.o." 
 
-#sns.catplot( x = represented_variable , y= category ,
-#             data=dfFractions , kind = k , orient='h',height=10, aspect=2 )
+sns.catplot( x = represented_variable , y= category ,
+             data=dfFractions , kind = k , orient='h',height=10, aspect=2 )
+plt.grid()
 
 # alternative for the curious
-
-sns.kdeplot( x = represented_variable , hue= category ,
-             data=dfFractions )
+#sns.kdeplot( x = represented_variable , hue= category ,
+#             data=dfFractions )
